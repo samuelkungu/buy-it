2fecd11a96ea2635a30e0e6a84fef3789a5f4d6a
Samuel <kungusamuel64@gmail.com>
2022-01-06 21:14:13 +0300

    Latest Updates

--- app/src/main/java/com/example/loginfirebase/LogIn.java
+++ app/src/main/java/com/example/loginfirebase/LogIn.java
@@ -13,30 +13,32 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn extends AppCompatActivity {
     Button logIn1;
-    EditText emailEd,passwordEd ;
+    EditText emailEd, passwordEd;
     TextView registerTxt;
 
     private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         logIn1 = findViewById(R.id.logInButton);
         registerTxt = findViewById(R.id.registerTxt);
-         emailEd = findViewById(R.id.email);
-         passwordEd = findViewById(R.id.logInPassword);
+        emailEd = findViewById(R.id.email);
+        passwordEd = findViewById(R.id.logInPassword);
 
 
         registerTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LogIn.this,MainActivity.class);
+                Intent intent = new Intent(LogIn.this, MainActivity.class);
                 startActivity(intent);
             }
         });
@@ -46,30 +48,38 @@ public class LogIn extends AppCompatActivity {
                 auth = FirebaseAuth.getInstance();
                 String email = emailEd.getText().toString().trim();
                 String password = passwordEd.getText().toString().trim();
-                if(email.isEmpty()){
+                if (email.isEmpty()) {
                     emailEd.setError("Email is required");
                     emailEd.requestFocus();
                     return;
                 }
-                if(password.isEmpty()){
+                if (password.isEmpty()) {
                     passwordEd.setError("password is required");
                     passwordEd.requestFocus();
                     return;
                 }
-                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LogIn.this, new OnCompleteListener<AuthResult>() {
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LogIn.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LogIn.this, "Successfully Logged in", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(LogIn.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(LogIn.this, "Error try again", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(LogIn.this, "Successfully Logged in", Toast.LENGTH_SHORT).show();
-                            Intent intent=new Intent(LogIn.this,MainActivity.class);
-                            startActivity(intent);
-                        }else{
-                            Toast.makeText(LogIn.this, "Error try again", Toast.LENGTH_SHORT).show();
-                        }
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "ERROR: " + e.toString(), Toast.LENGTH_LONG).show();
+
+
                     }
                 });
             }
-        });
-    }
 
+        }
+ );}
 }
